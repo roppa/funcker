1daffb3db20954797531c7cfea3e942c364d280c
Mark Robson <mark.o.robson@gmail.com>
2018-02-16 22:37:37 +0000

    Exported filter and only

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const get = require('./lib/get');
 const curry = require('./lib/curry');
+const filter = require('./lib/filter');
+const only = require('./lib/only');
 
-module.exports = { get, curry };
+module.exports = { get, curry, filter, only };
