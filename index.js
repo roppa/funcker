02b67b34897e2967034c4ad6589d4b367c140e60
Mark Robson <mark.o.robson@gmail.com>
2018-03-26 16:46:01 +0100

    refactor: updated lint. removing only side effects

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const curry = require('./lib/curry');
 const filter = require('./lib/filter');
 const only = require('./lib/only');
 
-module.exports = { get, curry, filter, only };
+module.exports = {
+  get, curry, filter, only,
+};
