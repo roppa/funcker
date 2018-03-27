1325d9893fe4bb4d16c4c69491fa4ebccfcc8cae
Mark Robson <mark.o.robson@gmail.com>
2018-03-27 19:33:54 +0100

    fix: expose compose

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const get = require('./lib/get');
 const curry = require('./lib/curry');
 const filter = require('./lib/filter');
 const only = require('./lib/only');
+const compose = require('./lib/compose');
 
 module.exports = {
-  get, curry, filter, only,
+  get, curry, filter, only, compose,
 };
